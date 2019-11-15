b07cfae99be526d2f8019cc753bfd5f1a101f63b
Richard G. Scates <richard.g.scates@disney.com>
2019-11-15 14:45:48 -0500

    Added fixes for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 
@@ -11,13 +12,20 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const db = require("./config/keys").mongoURI;
-mongoose.connect(db, { useNewUrlParser: true }).then(() => console.log("MongoDB connected")).catch((err) => console.log(err));
+const db = process.env.MONGODB_URI || require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true }).then(() => console.log('MongoDB connected')).catch((err) => console.log(err));
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
 app.use('/api/users', users);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
